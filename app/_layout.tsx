cdc59da708ebc643e3dce2761cb786dc3f4dd402
Marcus Oates <marcus.oates@downergroup.com>
2025-03-12 21:30:30 +1100

    check token logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,41 +1,76 @@
 import { Stack, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as SecureStore from "expo-secure-store";
+import { AppState } from "react-native";
+import { isTokenExpired } from "@/middleware/token";
 
 type AccountState = "not-registered" | "not-validated" | "expired" | "good" | null;
 
 export default function RootLayout() {
   const router = useRouter();
   const [accountState, setAccountState] = useState<AccountState>(null);
+  const shortTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const longTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  // useEffect(() => {
+  //   // todo implement proper token checking once signed up / in
+  //   const checkStorage = async () => {
+  //     try {
+  //       const token = await SecureStore.getItemAsync("long_token");
+  //       if (!token) {
+  //         setAccountState("not-registered");
+  //         return;
+  //       }
+
+  //       const url = process.env.EXPO_PUBLIC_API_URL as string;
+  //       const response = await fetch(`${url}/authenticate`, {
+  //         method: 'POST', 
+  //         body: JSON.stringify({
+  //           "token": token
+  //         })
+  //       });
+  //       if (!response.ok) throw new Error("HTTP error");
+        
+  //       const data = await response.json();
+
+  //     } catch (error) {
+  //       console.error("Error checking credentials:", error);
+  //       // show error message
+  //     }
+  //   };
+
+  //   checkStorage();
+  // }, []);
+
+  // if exists, load short token, if past half expiry refresh with long, else set timer for half expiry to refresh
+
+  const checkTokens = async () => {
+    const short_token = await SecureStore.getItemAsync("short_token");
+    const long_token = await SecureStore.getItemAsync("long_token");
+    if (!short_token && !long_token) {
+      router.replace("/sign-up");
+      return;
+    } else if (long_token !== null && isTokenExpired(long_token)) {
+      router.replace("/sign-in");
+      return;
+    }
+
+    // todo fetch is_validated endpoint
+    // todo if past half expiry, refresh tokens, else set timeout
+    // todo run this func onmount and appstate change to active
+
+  };
 
   useEffect(() => {
-    // todo implement proper token checking once signed up / in
-    const checkStorage = async () => {
-      try {
-        const token = await SecureStore.getItemAsync("auth_token_long");
-        if (!token) {
-          setAccountState("not-registered");
-          return;
-        }
-
-        const url = process.env.EXPO_PUBLIC_API_URL as string;
-        const response = await fetch(`${url}/authenticate`, {
-          method: 'POST',
-          body: JSON.stringify({
-            "token": token
-          })
-        });
-        if (!response.ok) throw new Error("HTTP error");
-        
-        const data = await response.json();
-
-      } catch (error) {
-        console.error("Error checking credentials:", error);
-        // show error message
+    const handleAppStateChange = (nextState: string) => {
+      if (nextState === "active") {
+        // startTimeout();
+        // start refresh cycle(s)
       }
     };
 
-    checkStorage();
+    const subscription = AppState.addEventListener("change", handleAppStateChange);
+    return () => subscription.remove();
   }, []);
 
   useEffect(() => {
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "expo-router";
 export default function SignInScreen() {
   const router = useRouter();
   
+  // todo allow fingerprint or face
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "black"}}>
       <Text style={{ color: "white"}}>Sign In</Text>
