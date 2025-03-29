6f1dcfb9e6b7f037bedef2cd6d9fa8170fff09d7
Marcus Oates <marcus.oates@downergroup.com>
2025-03-29 16:26:20 +1100

    refactor for validation chnage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,14 @@
 import { Stack, useRouter } from "expo-router";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import * as SecureStore from "expo-secure-store";
-import { AppState } from "react-native";
-import { isTokenExpired } from "@/middleware/token";
 import { jwtDecode } from "jwt-decode";
-
-type AccountState = "not-registered" | "not-validated" | "expired" | "good" | null;
 
 export default function RootLayout() {
   const router = useRouter();
-  const [accountState, setAccountState] = useState<AccountState>(null);
-
-  // const checkTokens = async () => {
-  //   const short_token = await SecureStore.getItemAsync("short_token");
-  //   const long_token = await SecureStore.getItemAsync("long_token");
-  //   if (!short_token && !long_token) {
-  //     router.replace("/sign-up");
-  //     return;
-  //   } else if (long_token !== null && isTokenExpired(long_token)) {
-  //     router.replace("/sign-in");
-  //     return;
-  //   }
-
-  //   // todo fetch is_validated endpoint
-  //   // todo if past half expiry, refresh tokens, else set timeout
-  //   // todo run this func onmount and appstate change to active
-
-  // };
-
-  // useEffect(() => {
-  //   const handleAppStateChange = (nextState: string) => {
-  //     if (nextState === "active") {
-  //       // startTimeout();
-  //       // start refresh cycle(s)
-  //     }
-  //   };
-
-  //   const subscription = AppState.addEventListener("change", handleAppStateChange);
-  //   return () => subscription.remove();
-  // }, []);
 
   useEffect(() => {
     router.replace("/loading");
-    
+
     const checkUserState = async () => {
       const auth_token = await SecureStore.getItemAsync("auth_token");
       if (!auth_token) {
@@ -50,10 +16,9 @@ export default function RootLayout() {
         return;
       }
 
-      // todo new login route that returns user data to client
-
       try {
-        const decoded: {email: string } = jwtDecode(auth_token);
+        const decoded: {email: string, exp: number, iat: number } = jwtDecode(auth_token);
+        
         const response = await fetch(
           `${process.env.EXPO_PUBLIC_API_URL}/register/validate/check?` + 
           new URLSearchParams({ "email": decoded.email }).toString()
@@ -61,9 +26,17 @@ export default function RootLayout() {
         if (!response.ok) throw new Error("response not ok");
 
         const data = await response.json();
-        if (data.is_validated !== true) {
-          router.replace("/validate");
+        if (data.account_state == "none") {
+          router.replace("/sign-in")
+        } else if (data.account_state == "unverified") {
+          router.replace("/validate")
+        } else if (data.account_state == "good") {
+          await SecureStore.setItemAsync("auth_token", data.auth_token);
+          router.replace("/(tabs)");
+        } else {
+          throw new Error("response ot recognised");
         }
+
       } catch (error) {
         console.log(error);
         router.replace("/sign-up")
--- app/validate.tsx
+++ app/validate.tsx
@@ -7,18 +7,20 @@ export default function Validate() {
   const { email } = useLocalSearchParams();
   const emailString: string = Array.isArray(email) ? email[0] : email ?? "";
 
+  const pathname = usePathname();
   const router = useRouter();
-  const [stopFetch, SetStopFetch] = useState<boolean>(false); 
+  const [stopFetch, setStopFetch] = useState<boolean>(true); 
 
-  // TODO prevent timeout until on page
+  useEffect(() => {
+    setStopFetch(pathname !== '/validate');
+  }, [pathname]);
 
   useEffect(() => {
     let timeoutId: NodeJS.Timeout;
 
     const fetchValidationStatus = async () => {
       if (stopFetch) return;
-      console.log(Date.now())  
-      
+
       let is_validated: boolean = false;
       try {
         const response = await fetch(
@@ -29,12 +31,15 @@ export default function Validate() {
 
         const data = await response.json();
 
-        if (data["is_verified"] === true) {
+        if (data.account_state === "good") {
           is_validated = true;
+          clearTimeout(timeoutId);
           await SecureStore.setItemAsync('auth_token', data.auth_token);
           router.replace("/(tabs)")
-          return;
+        } else if (data.account_state === "none") {
+          throw new Error("email not recognised")
         }
+
       } catch (error) {
         console.log(error);
       } finally {
@@ -55,7 +60,7 @@ export default function Validate() {
       <Text style={{ color: "white" }}>Validate your email</Text>
       <TouchableOpacity 
         onPress={() => {
-          SetStopFetch(true);
+          setStopFetch(true);
           router.replace("/sign-up");
         }}
       >
