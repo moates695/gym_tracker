0ab2129ac8fce39a419e3859ee0f32ca5e8b24c5
Marcus Oates <marcus.oates@downergroup.com>
2025-06-07 11:50:37 +1000

    dropdown ontop of other elements

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -22,24 +22,45 @@ export default function Dropdown(props: DropdownProps) {
     setSelectedIdx(index);
   };
 
+  const isLastOption = (index: number): boolean => {
+    if (selectedIdx === options.length - 1) {
+      return index === options.length - 2;
+    }
+    return index === options.length - 1;
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity
         onPress={() => setIsOpen(!isOpen)}
+        style={[styles.optionContainer, {'padding': 5}]}
       >
         <Text style={styles.text}>{options[selectedIdx].label}</Text>
       </TouchableOpacity>
-      {isOpen && options.map((option, index) => (
-        <View key={index}>
-          { index !== selectedIdx &&
-            <TouchableOpacity 
-              onPress={() => handleSelectOption(index)}
-            >
-              <Text style={styles.text}>{option.label}</Text>
-            </TouchableOpacity>
-          }
+      {isOpen && 
+        <View 
+          style={[styles.optionContainer, styles.dropdownContainer]}>
+          {options.map((option, index) => (
+            <View key={index}>
+              {index !== selectedIdx &&
+                <View
+                  style={[
+                    styles.dropdownOption,
+                    !isLastOption(index) && styles.optionDivider,
+                    {padding: 5}
+                  ]}
+                >
+                  <TouchableOpacity 
+                    onPress={() => handleSelectOption(index)}
+                  >
+                    <Text style={styles.text}>{option.label}</Text>
+                  </TouchableOpacity>
+                </View>
+              }
+            </View>
+          ))}
         </View>
-      ))}
+      }
     </View>
   )
 }
@@ -49,6 +70,29 @@ const styles = StyleSheet.create({
     color: 'white'
   },
   container: {
-    flex: 0
+    flex: 0,
+    width: 150
+  },
+  optionDivider: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    height: 30
+  },
+  optionContainer: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 2,
+    // padding: 5,
+    justifyContent: 'center',
+  },
+  dropdownContainer: {
+    zIndex: 999,
+    top: 28,
+    backgroundColor: 'black',
+    position: 'absolute',
+    width: 150,
+  },
+  dropdownOption: {
+    padding: 3,
   }
 })
