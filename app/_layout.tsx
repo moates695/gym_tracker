3b9696a5586d3dd6e541aa7a9652b28929b0a6e5
Marcus Oates <marcus.oates@downergroup.com>
2025-10-03 19:13:11 +1000

    save progress

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -109,6 +109,7 @@ export default function RootLayout() {
             fetchHistoryStats(),
             fetchWorkoutTotalStats(),
             fetchDistributionStats(),
+            fetchFavouriteStats(),
           ])
         } catch (error) {
           console.log(error);
