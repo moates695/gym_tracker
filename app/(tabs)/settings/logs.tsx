397a18202b05840a217a9398a04b07647bbc05b6
Marcus Oates <marcus.oates@downergroup.com>
2025-12-05 07:53:09 +1100

    able to delete logs

--- app/(tabs)/settings/logs.tsx
+++ app/(tabs)/settings/logs.tsx
@@ -1,9 +1,11 @@
 import LoadingScreen from "@/app/loading";
+import ConfirmationModal from "@/components/ConfirmationModal";
 import { useAwaitLoadable } from "@/middleware/helpers";
-import { loadableErrorLogsAtom } from "@/store/general";
+import { errorLogsAtom, loadableErrorLogsAtom } from "@/store/general";
 import { commonStyles } from "@/styles/commonStyles";
-import React from "react";
-import { FlatList, View, Text } from "react-native";
+import { useSetAtom } from "jotai";
+import React, { useState } from "react";
+import { FlatList, View, Text, TouchableOpacity } from "react-native";
 
 // todo clear logs button with confirmation
 
@@ -12,6 +14,14 @@ export default function LogsPage() {
     value: errorLogs, 
     isReady: errorLogsReady 
   } = useAwaitLoadable(loadableErrorLogsAtom, []); 
+
+  const setErrorLogs = useSetAtom(errorLogsAtom);
+  const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
+
+  const handleDeleteLogs = () => {
+    setErrorLogs([]);
+    setShowConfirmation(false);
+  };
 
   if (!errorLogsReady) {
     return <LoadingScreen />
@@ -26,39 +36,68 @@ export default function LogsPage() {
         paddingRight: 10,
       }}
     >
-      <FlatList 
-        style={{
-          marginTop: 10,
-          marginBottom: 10,
-        }}
-        data={errorLogs}
-        renderItem={({ item, index }) => (
-          <View 
+      {errorLogs.length === 0 ?
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Text style={commonStyles.text}>no logs yet</Text>
+        </View>
+      :
+        <>
+          <TouchableOpacity
+            style={[commonStyles.thinTextButton, {width: 50, marginLeft: 12}]}
+            onPress={() => setShowConfirmation(true)}
+            disabled={errorLogs.length === 0}
+          >
+            <Text style={commonStyles.text}>delete</Text>
+          </TouchableOpacity>
+          <FlatList 
             style={{
-              flexDirection: 'row', 
-              marginBottom: 4,
-              backgroundColor: index % 2 ? '#000000' : '#222328ff',
-              borderRadius: 5,
-              padding: 5,
-            }}>
-            <View style={{width: 155}}>
-              <Text style={[
-                commonStyles.text,
-                item.level === 'error' ? {
-                  color: 'red'
-                } : {
-                  color: 'orange'
-                }]}>
-                {item.date_str}
-              </Text>
-            </View>
-            <Text style={[commonStyles.text, {flex: 1}]}>
-              {item.message}
-            </Text>
-          </View>
-        )}
-        showsVerticalScrollIndicator={true}
-      />
+              marginTop: 10,
+              marginBottom: 10,
+            }}
+            data={errorLogs}
+            renderItem={({ item, index }) => (
+              <View 
+                style={{
+                  flexDirection: 'row', 
+                  marginBottom: 4,
+                  backgroundColor: index % 2 ? '#000000' : '#222328ff',
+                  borderRadius: 5,
+                  padding: 5,
+                }}>
+                <View style={{width: 155}}>
+                  <Text style={[
+                    commonStyles.text,
+                    item.level === 'error' ? {
+                      color: 'red'
+                    } : {
+                      color: 'orange'
+                    }]}>
+                    {item.date_str}
+                  </Text>
+                </View>
+                <Text style={[commonStyles.text, {flex: 1}]}>
+                  {item.message}
+                </Text>
+              </View>
+            )}
+            showsVerticalScrollIndicator={true}
+          />
+          <ConfirmationModal 
+            visible={showConfirmation}
+            onConfirm={handleDeleteLogs}
+            onCancel={() => setShowConfirmation(false)}
+            message='Delete all logs?'
+            confirm_string="yep"
+            cancel_string="nah"
+          />
+        </>
+      }
     </View>
   )
 }
