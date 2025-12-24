b701b2d04e644b993b5c865265fb810c70794e40
Marcus Oates <marcus.oates@downergroup.com>
2025-12-24 17:01:38 +1100

    updated

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,3 +1,6 @@
+import { OptionsObject } from "@/components/ChooseExerciseModal";
+import { TimeSpanOption, TimeSpanOptionObject, useDropdown } from "@/components/ExerciseData";
+import MuscleGroupSvg from "@/components/MuscleGroupSvg";
 import { fetchWrapper } from "@/middleware/helpers";
 import { addCaughtErrorLogAtom, addErrorLogAtom } from "@/store/actions";
 import { homeMuscleHistoryAtom } from "@/store/general";
@@ -20,6 +23,23 @@ export default function Home() {
 
   const [homeMuscleHistory, setHomeMuscleHistory] = useAtom(homeMuscleHistoryAtom);
   const [loadingData, setLoadingData] = useState<boolean>(false);
+
+  const timeSpanOptions: TimeSpanOptionObject[] = [
+    { label: 'week', value: 'week' },
+    { label: 'month', value: 'month' },
+    { label: '3 months', value: '3_months' },
+    { label: '6 months', value: '6_months' },
+    { label: 'year', value: 'year' },
+    { label: 'all', value: 'all' },
+  ]
+  const [timeSpanValue, setTimeSpanValue] = useState<TimeSpanOption>('month');
+
+  const metricOptions: OptionsObject[] = [
+    { label: 'volume', value: 'volume' },
+    { label: 'sets', value: 'sets' },
+    { label: 'reps', value: 'reps' },
+  ]
+  const [metricValue, setMetricValue] = useState<string>('volume');
 
   const addErrorLog = useSetAtom(addErrorLogAtom);
   const addCaughtErrorLog = useSetAtom(addCaughtErrorLogAtom)
@@ -47,22 +67,72 @@ export default function Home() {
     loadData();
   }, []);
 
+  const getValueMap = (): Record<string, number> => {
+    const map: Record<string, number> = {};
+    if (homeMuscleHistory === null) return map;
+
+    try {
+      const data = homeMuscleHistory[timeSpanValue];
+      for (const [groupName, groupStats] of Object.entries(data)) {
+        const stats = groupStats as Record<string, any>;
+        if (stats[metricValue] === 0) continue;
+        map[groupName] = stats[metricValue];
+        for (const [targetName, targetStats] of Object.entries(stats["targets"])) {
+          const targetStat = targetStats as Record<string, any>;
+          if (targetStat[metricValue] === 0) continue;
+          map[`${groupName}/${targetName}`] = targetStat[metricValue];
+        }
+      }
+
+      // for (const [groupName, groupStats] of Object.entries(distributions)) {
+      //   if (heatmapDisplayValue === 'group') {
+      //     if (groupStats[metricOptionValue] === 0) continue;
+      //     map[groupName] = groupStats[metricOptionValue];
+      //     continue;
+      //   } 
+      //   for (const [targetName, targetStats] of Object.entries(groupStats["targets"])) {
+      //     if (targetStats[metricOptionValue] === 0) continue;
+      //     map[`${groupName}/${targetName}`] = targetStats[metricOptionValue];
+      //   }
+      // }
+    } catch (error) {
+      addCaughtErrorLog(error, 'error building value map');
+    }
+    return map;
+  }
+
   return (
-    <ScrollView
+    <View
       style={{
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
-    >        
+    >       
+      <View>
+        <Text style={commonStyles.text}>Choose a view:</Text>
+        {useDropdown(timeSpanOptions, timeSpanValue, setTimeSpanValue)}
+      </View> 
+      <View>
+        <Text style={commonStyles.text}>Choose a metric:</Text>
+        {useDropdown(metricOptions, metricValue, setMetricValue)}
+      </View> 
       {/* <TouchableOpacity
         style={[styles.button, {width: 200}]}
         onPress={() => router.replace('/(tabs)/home/friends')}
       >
         <Text style={commonStyles.text}>friends</Text>
       </TouchableOpacity> */}
-      <Text style={{color: "white", marginTop: 20}}>home coming soon...</Text>
+      <View style={{marginBottom: 20, marginTop: 20}}>
+        <MuscleGroupSvg 
+          valueMap={getValueMap()} 
+          showGroups={false}
+          // showGroups={heatmapDisplayValue === 'group'}
+        />
+      </View>
       <StatusBar style='dark' />
-    </ScrollView>        
+    </View>        
   );
 }
 
